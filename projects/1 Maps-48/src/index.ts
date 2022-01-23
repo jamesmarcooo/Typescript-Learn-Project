102d2316746ba929dd6e75d4e27256cd66b5d0ec
Marco <james.marco.reyes@eee.upd.edu.ph>
2022-01-23 10:57:20 +0800

    Fixed namespace google bug

--- projects/1 Maps-48/src/index.ts
+++ projects/1 Maps-48/src/index.ts
@@ -1,3 +1,11 @@
+/// <reference path="../node_modules/@types/google.maps/index.d.ts" />
 import { User } from "./User";
 import { Company } from "./Company";
-import { getParsedCommandLineOfConfigFile } from "typescript";
+
+const map = new google.maps.Map(document.getElementById("map"), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
