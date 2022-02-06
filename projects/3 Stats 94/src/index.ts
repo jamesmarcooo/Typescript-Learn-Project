eff6605223b20fe67276b5c346128628d1d2ca8c
Marco <james.marco.reyes@eee.upd.edu.ph>
2022-02-06 10:42:57 +0800

    Added a CsvFileReader class

--- projects/3 Stats 94/src/index.ts
+++ projects/3 Stats 94/src/index.ts
@@ -1,1 +1,10 @@
-console.log("initializing...");
+import { CsvFileReader } from "./CsvFileReader";
+
+const reader = new CsvFileReader("football.csv");
+
+//enum
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D",
+}
